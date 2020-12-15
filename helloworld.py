2160ad894e0715e909e37419d5ff9640d564422d
Shani <35404965+Shani48@users.noreply.github.com>
2020-12-15 13:58:08 +0500

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,6 +0,0 @@
-#first programme
-#Hello world in Python
-print("Hello World")
-
-#print() is a function which use for the printing key word in python
-#The print() function prints the specified message to the screen
