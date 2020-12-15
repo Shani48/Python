5d15cc446d1c4e4d10f4c2de40630ea812cf1324
Shani <35404965+Shani48@users.noreply.github.com>
2020-12-15 13:52:41 +0500

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+#first programme
+#Hello world in Python
+print("Hello World")
+
+#print() is a function which use for the printing key word in python
+#The print() function prints the specified message to the screen
